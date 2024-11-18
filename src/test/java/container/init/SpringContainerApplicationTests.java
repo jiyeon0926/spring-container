207c9a03a0728428ebf2d8559ae7cf07bdd0b9e7
Yeon <yeoni9094@naver.com>
2024-11-18 15:54:34 +0900

    init

---
+++ src/test/java/container/init/SpringContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package container.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringContainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
